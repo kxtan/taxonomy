6aa1794995878e548b675d866cfacf6627e77193
kxuen <tkxuen@gmail.com>
2021-12-08 00:31:52 +0800

    update sample test case

--- tests/test_util.py
+++ tests/test_util.py
@@ -7,11 +7,11 @@ sys.path.append("..")
 from modules import util
 
 def test_read_file():
-    file = util.read_file("data/test_sample.tsv")
+    file = util.read_file("./data/test_sample.tsv")
     assert file is not None
 
 def test_generate_text_dict():
-    df = util.read_file("data/test_sample.tsv")
+    df = util.read_file("./data/test_sample.tsv")
     dct = util.generate_text_dict(df, "id", "text")
 
     assert type(dct) == dict
